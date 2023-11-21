1335f86bb412c26f4de728544f7fa6a89fd0c9e2
AdrianPauer <118552046+AdrianPauer@users.noreply.github.com>
2023-11-21 16:24:04 +0100

    feature od NR values + VRS added

--- models.py
+++ models.py
@@ -2,12 +2,20 @@ import cvxpy as cp
 import numpy as np
 import pandas as pd
 
+
 class Model:
-    def __init__(self, inputs, outputs):
+    def __init__(self, inputs, outputs, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
         self.X = inputs
         self.Y = outputs
+
         self.m, self.n, self.s = None, None, None
+        
+        self.vrs = vrs
+        self.nonDisctretionaryInputs = nonDiscretionaryInputs
+        self.nonDisctretionaryOutputs = nonDiscretionaryOutputs
+
         self.initialize_params()
+        self.controlData()
 
         self.objective = None
         self.constraints = []
@@ -16,6 +24,33 @@ class Model:
         self.problem = None
         self.solver = None
         self.solutions = dict()
+
+    def controlData(self):
+        if self.X.ndim == 1 and  isinstance(self.X[0],list) :
+            raise ValueError('Input matrix is incomplete')
+        if self.Y.ndim == 1 and  isinstance(self.Y[0],list) :
+            raise ValueError('Output matrix is incomplete')
+        if (self.X <= 0).sum() > 0 or (self.Y <= 0).sum() > 0 :
+            raise ValueError('All data should be positive')
+        
+        if self.nonDisctretionaryInputs is not None :
+            if ((self.nonDisctretionaryInputs >= self.m) | (self.nonDisctretionaryInputs < 0)).sum() > 0 : 
+                raise IndexError('Index for nondiscretionary inputs should be in range [0,m-1]')
+        if self.nonDisctretionaryOutputs is not None :
+            if ((self.nonDisctretionaryOutputs >= self.s) | (self.nonDisctretionaryOutputs < 0)).sum() > 0 : 
+                raise IndexError('Index for nondiscretionary inputs should be in range [0,s-1]')
+    
+    def __str__(self):
+        if self.solutions is None : return 'model with no solutions'
+        solutionsFrame = pd.DataFrame.from_dict(self.solutions, orient='index')
+        solutionsFrame.index.name = 'DMU'
+        return solutionsFrame.__str__()
+    
+
+    def scaleData(self):
+        self.X = self.X/self.X.max()
+        self.Y = self.Y/self.Y.max()
+    
 
     def initialize_params(self):
         self.m = 1 if self.X.ndim == 1 else  self.X.shape[0]
@@ -30,8 +65,21 @@ class Model:
     def fill_constraints_slack_based(self, x_0 , y_0):
         self.constraints = [self.X @ self.l == x_0 - self.s_x,
                             self.Y @ self.l == y_0 + self.s_y,
-                            cp.sum(self.l) == 1,
                             self.s_x >= 0, self.s_y >= 0, self.l >= 0]
+        if self.vrs : self.constraints.append(cp.sum(self.l) == 1)
+        
+        if self.nonDisctretionaryInputs is not None :
+            if self.nonDisctretionaryInputs.shape[0] > 0 and self.nonDisctretionaryInputs.shape[0] <= self.m:
+                self.constraints.append(self.s_x[self.nonDisctretionaryInputs] == 0)
+            else : print('nonDiscInputsViolation')
+
+        if self.nonDisctretionaryOutputs is not None :
+            if self.nonDisctretionaryOutputs.shape[0] > 0 and self.nonDisctretionaryOutputs.shape[0] <= self.s:
+                self.constraints.append(self.s_y[self.nonDisctretionaryOutputs] == 0)
+            else : print('nonDiscOutputsViolation')
+        
+    def get(self, key):
+        return np.array(list(map(lambda x : x[key], list(self.solutions.values()))))
         
     def solve(self):
 
@@ -42,44 +90,64 @@ class Model:
             self.fill_constrainsts(x_0, y_0)
  
             self.problem.solve(solver = self.solver)
-            self.store_results(i+1)
+            self.store_results(i+1,x_0,y_0)        
+        self.writeSolutionsToFile()
+        return True
 
-        solutions_Frame = pd.DataFrame.from_dict(self.solutions, orient='index')
-        solutions_Frame.index.name = 'DMU'
-        print(solutions_Frame)
     
-    def store_results(self,which_dmu) :
+    def store_results(self,which_dmu,x_0,y_0) :
         self.solutions[which_dmu] = dict()
         for variable,key in (self.problem,'solution'),(self.s_x,'slack_x'), (self.s_y,'slack_y'), (self.l,'lambda'):
-            self.solutions[which_dmu][key] = variable.value
+            self.solutions[which_dmu][key] = np.round(variable.value,6)
+        self.solutions[which_dmu]['projectionX'] = np.round(self.X @ self.l.value,6)
+        self.solutions[which_dmu]['projectionY'] = np.round(self.Y @ self.l.value,6)
+
+        self.solutions[which_dmu]['partialEff_inputs'] =   np.round(1 - 1/x_0 * self.s_x.value,6)
+        self.solutions[which_dmu]['partialEff_outputs'] =   np.round(1 + 1/y_0 * self.s_y.value,6)
+    
+    def writeSolutionsToFile(self):
+        with open('output.txt','w') as outputFile:
+            for key in self.solutions.keys():
+                outputFile.write(str(key) + '\n')
+
+                for key2,value in self.solutions[key].items():
+                    
+                    if isinstance(value,np.ndarray): 
+                        outputFile.write(' '.join(list(map(str,value))) + '\n')
+                    else : outputFile.write(str(value) + '\n')
+        outputFile.close()
+
+
+
+    
+
 
 class AdditiveModel(Model):
-    def __init__(self, inputs, outputs):
-        Model.__init__(self,inputs, outputs)
+    def __init__(self, inputs, outputs, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
+        Model.__init__(self,inputs, outputs,vrs, nonDiscretionaryInputs, nonDiscretionaryOutputs)
         Model.init_variables_slack_based(self)
 
         self.objective = lambda s_x, s_y : 1 - (cp.sum(s_x) + cp.sum(s_y))
+
+        print('Result might be inacurete due to working with nonScaledData')
     
     def fill_constrainsts(self,x_0,y_0):
         Model.fill_constraints_slack_based(self, x_0, y_0)
         self.problem = cp.Problem(cp.Minimize(self.objective(self.s_x, self.s_y)),self.constraints)
-    
-    def store_results(self,which_dmu) :
-        self.solutions[which_dmu] = dict()
-        for variable,key in (self.problem,'solution'),(self.s_x,'slack_x'), (self.s_y,'slack_y'), (self.l,'lambda'):
-            self.solutions[which_dmu][key] = variable.value
-
+        
 
     
 
 
 class WeightedAdditiveModel(Model):
-    def __init__(self, inputs, outputs, inputWeights, outputWeights):
-        Model.__init__(self,inputs, outputs)
+    def __init__(self, inputs, outputs, inputWeights, outputWeights, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
+        Model.__init__(self,inputs, outputs, vrs, nonDiscretionaryInputs, nonDiscretionaryOutputs)
         Model.init_variables_slack_based(self)
 
         self.w_x,self.w_y = inputWeights, outputWeights
         self.objective = lambda s_x, s_y, w_x, w_y :  1 - (w_x @ s_x + w_y @ s_y)
+
+        print('Result might be inacurete due to working with nonScaledData')
     
     def fill_constrainsts(self,x_0,y_0):
         Model.fill_constraints_slack_based(self, x_0, y_0)
@@ -87,9 +155,10 @@ class WeightedAdditiveModel(Model):
     
 
 class MeasureEfficiencyProportionModel(Model):
-    def __init__(self, inputs, outputs):
-        Model.__init__(self,inputs, outputs)
+    def __init__(self, inputs, outputs, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
+        Model.__init__(self,inputs, outputs,vrs, nonDiscretionaryInputs, nonDiscretionaryOutputs)
         Model.init_variables_slack_based(self)
+        self.scaleData()
         self.objective = lambda s_x, s_y, x_0, y_0: 1 - (1/(self.m + self.s)) * (cp.sum(s_x/x_0) + cp.sum(s_y/y_0) )
     
     def fill_constrainsts(self,x_0,y_0):
@@ -97,9 +166,10 @@ class MeasureEfficiencyProportionModel(Model):
         self.problem = cp.Problem(cp.Minimize(self.objective(self.s_x, self.s_y,x_0,y_0)),self.constraints)
     
 class BoundedAdjustedMeasureModel(Model):
-    def __init__(self, inputs, outputs):
-        Model.__init__(self,inputs, outputs)
+    def __init__(self, inputs, outputs, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
+        Model.__init__(self,inputs, outputs,vrs, nonDiscretionaryInputs, nonDiscretionaryOutputs)
         Model.init_variables_slack_based(self)
+        self.scaleData()
 
         self.objective = lambda s_x,s_y,x_0,y_0 : self.objective_function(s_x,s_y,x_0,y_0)
     
@@ -127,8 +197,9 @@ class BoundedAdjustedMeasureModel(Model):
     
 
 class RusselModel(Model):
-    def __init__(self, inputs, outputs):
-        Model.__init__(self,inputs, outputs)
+    def __init__(self, inputs, outputs, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
+        Model.__init__(self,inputs, outputs,vrs, nonDiscretionaryInputs, nonDiscretionaryOutputs)
+        self.scaleData()
         self.l =  cp.Variable(self.n)
         self.tetha, self.psi, self.phi = cp.Variable(self.m),cp.Variable(self.s),cp.Variable(self.s)
 
@@ -141,6 +212,8 @@ class RusselModel(Model):
                             self.phi - np.ones(self.s) >= np.zeros(self.s),
                             self.l >= np.zeros(self.n)]
         
+        if self.vrs : self.constraints.append(cp.sum(self.l) == 1)
+        
         if self.m == 1 :
             self.constraints.append(-self.X@self.l + x_0 * self.tetha >= np.zeros(self.m))
         else : self.constraints.append(-self.X@self.l + np.diag(x_0)@self.tetha >= np.zeros(self.m))
@@ -151,27 +224,39 @@ class RusselModel(Model):
         else : self.constraints.append(self.Y@self.l  - np.diag(y_0)@self.phi >= np.zeros(self.s))
 
         
-        for r in range(self.s):
-            self.constraints.append(cp.bmat([ [self.psi[r], 1],
-                                              [1, self.phi[r]] ]) >> 0)
+        self.constraints.append(cp.bmat( [[cp.diag(self.psi),np.eye(self.s)], [np.eye(self.s),cp.diag(self.phi)] ]) >> 0)
+            
+        if self.nonDisctretionaryInputs is not None :
+            if self.nonDisctretionaryInputs.shape[0] > 0 and self.nonDisctretionaryInputs.shape[0] <= self.m:
+                self.constraints.append(self.tetha[self.nonDisctretionaryInputs] == 1)
+            else : print('nonDiscInputsViolation')
+
+        if self.nonDisctretionaryOutputs is not None :
+            if self.nonDisctretionaryOutputs.shape[0] > 0 and self.nonDisctretionaryOutputs.shape[0] <= self.s:
+                self.constraints.append(self.phi[self.nonDisctretionaryOutputs] == 1)
+            else : print('nonDiscOutputsViolation')
+        
             
         self.problem = cp.Problem(cp.Minimize(self.objective(self.tetha, self.psi)),self.constraints)
+        
     
-    def store_results(self, which_dmu):
+    def store_results(self, which_dmu,x_0, y_0):
         self.solutions[which_dmu] = dict()
         for variable,key in (self.problem,'solution'),(self.tetha,'tetha'), (self.phi,'phi'), (self.psi,'psi'), (self.l,'lambda'):
-            self.solutions[which_dmu][key] = variable.value
+            self.solutions[which_dmu][key] = np.round(variable.value,4)
 
+        self.solutions[which_dmu]['projectionX'] = np.round(self.X @ self.l.value,6)
+        self.solutions[which_dmu]['projectionY'] = np.round(self.Y @ self.l.value,6)
+        
+        self.solutions[which_dmu]['partialEff_inputs'] =  np.round(self.tetha.value,4)
+        self.solutions[which_dmu]['partialEff_outputs'] =  np.round(1/self.phi.value,4)
     
     
-
-    
-    
-
 class SlackedBasedMeausereModel(Model):
-    def __init__(self, inputs, outputs):
-        Model.__init__(self,inputs, outputs)
+    def __init__(self, inputs, outputs, vrs = False, nonDiscretionaryInputs = None, nonDiscretionaryOutputs = None):
+        Model.__init__(self,inputs, outputs,vrs, nonDiscretionaryInputs, nonDiscretionaryOutputs)
         Model.init_variables_slack_based(self)
+        self.scaleData()
         
         self.t = cp.Variable(1)
         self.objective = lambda t,x_0 : t - (1/self.m *cp.sum(self.s_x/x_0)) 
@@ -181,12 +266,31 @@ class SlackedBasedMeausereModel(Model):
         self.constraints = [ self.t + 1/self.s * cp.sum(self.s_y/y_0) == 1,
                             self.X@self.l + self.s_x == self.t * x_0,
                             self.Y@self.l - self.s_y == self.t * y_0,
-                            cp.sum(self.l) == self.t,
                             self.s_x >= 0, self.s_y >= 0, self.l >= 0]
+        
+        if self.vrs : self.constraints.append(cp.sum(self.l) == self.t)
+
+        if self.nonDisctretionaryInputs is not None :
+            if self.nonDisctretionaryInputs.shape[0] > 0 and self.nonDisctretionaryInputs.shape[0] <= self.m:
+                self.constraints.append(self.s_x[self.nonDisctretionaryInputs] == 0)
+            else : print('nonDiscInputsViolation')
+
+        if self.nonDisctretionaryOutputs is not None :
+            if self.nonDisctretionaryOutputs.shape[0] > 0 and self.nonDisctretionaryOutputs.shape[0] <= self.s:
+                self.constraints.append(self.s_y[self.nonDisctretionaryOutputs] == 0)
+            else : print('nonDiscOutputsViolation')
+        
+        
         self.problem = cp.Problem(cp.Minimize(self.objective(self.t, x_0)),self.constraints)
+
     
-    def store_results(self, which_dmu):
+    def store_results(self, which_dmu,x_0,y_0):
         self.solutions[which_dmu] = dict()
         for variable,key in (self.problem,'solution'),(self.s_x,'slack_x'), (self.s_y,'slack_y'), (self.t,'t'),(self.l,'lambda'):
-            self.solutions[which_dmu][key] = variable.value
+            self.solutions[which_dmu][key] = np.round(variable.value,6)
+        self.solutions[which_dmu]['projectionX'] = np.round(self.X @ self.l.value,6)
+        self.solutions[which_dmu]['projectionY'] = np.round(self.Y @ self.l.value,6)
+
+        self.solutions[which_dmu]['partialEff_inputs'] =   np.round(1 - 1/x_0 * self.s_x.value,6)
+        self.solutions[which_dmu]['partialEff_outputs'] =   np.round(1 + 1/y_0 * self.s_y.value,6)
     
